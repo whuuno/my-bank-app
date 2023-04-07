cd3acc94416f0a79576935ef106b96a8dfcc42ea
Panks <pankajsingla012@gmail.com>
2023-04-07 14:19:24 +0530

    Creating Modules

---
+++ cards/cards/src/test/java/com/practice/cards/CardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
