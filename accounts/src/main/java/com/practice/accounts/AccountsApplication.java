3aabaa657eb0b6b06578926bfc6203e5582de23f
Panks <pankajsingla012@gmail.com>
2023-06-03 20:45:46 +0530

    Adding RefreshScope for clients and encryption for configserver

--- accounts/src/main/java/com/practice/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/practice/accounts/AccountsApplication.java
@@ -3,12 +3,14 @@ package com.practice.accounts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@RefreshScope
 @ComponentScans({ @ComponentScan("com.practice.accounts.controller") })
 @EnableJpaRepositories("com.practice.accounts.repository")
 @EntityScan("com.practice.accounts.model")
